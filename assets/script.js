a6a673a2b794a13858f5c94b7132886ae10a93d6
Alejandra Izquierdo <allleizq@gmail.com>
2022-06-28 15:40:39 -0700

    added css stying & functions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
-var startButton = document.getElementById("startbutton")
-var timerElement = document.querySelector(".timer");
+var startButton = document.querySelector("#startbutton");
+var timerElement = document.querySelector("#timer");
+var highScores = document.querySelector("#highscores");
+var questionContainer = document.querySelector("#questionsection")
+var questions = document.querySelector("#questions");
+var answers = document.querySelector("#answers");
+var nextButton = document.querySelector("#countinuebutton")
+var startingBox = document.querySelector(".startingbox");
+var quizBox = document.querySelector("#quizbox");
 
 startButton.addEventListener("click", startsQuiz)
 
@@ -22,45 +29,49 @@ startTimer();
 
 function startsQuiz() {
     console.log("begin")
+    startingBox.classList.add("deletes");
     
     goesToNextQ();
     startTimer();
 }
 
+
 function goesToNextQ() {
-    
+    quizBox.classList.remove("deletes");
+    displayQuestion(questionsArray[0])
+    questionClass.innerHTML = questionsArray.question
+
 }
 
 function selectsAnswer() {
 
 }
 
-var question1 = [
-  {
+var questionsArray = [{
     question: "Which of the following is not a popup box in Javascript?",
     answers: [
-        { text: "prompt", incorrect: false },
-        { text: "array", incorrect: true },
-        { text: "alert", incorrect: false },
-        { text: "confirm", incorrect: false },
+        { text: "prompt", isCorrect: false },
+        { text: "array", isCorrect: true },
+        { text: "alert", isCorrect: false },
+        { text: "confirm", isCorrect: false }
     ]
   },   
   {
     question: "Which of the following is a tag?",
     answers: [
-        { text: "div", incorrect: true },
-        { text: "array", incorrect: false },
-        { text: "alert", incorrect: false },
-        { text: "scope", incorrect: false },
+        { text: "div", isCorrect: true },
+        { text: "array", isCorrect: false },
+        { text: "alert", isCorrect: false },
+        { text: "scope", isCorrect: false }
     ]
   }, 
   {  
   question: "Which of the following is used to style a webpage?",
     answers: [
-        { text: "emojis", incorrect: true },
-        { text: "functions", incorrect: false },
-        { text: "methods", incorrect: false },
-        { text: "CSS", incorrect: false },
+        { text: "emojis", isCorrect: true },
+        { text: "functions", isCorrect: false },
+        { text: "methods", isCorrect: false },
+        { text: "CSS", isCorrect: false }
     ]
   }   
 ]
