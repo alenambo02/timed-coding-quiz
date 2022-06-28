026509199d0566b864dc7e2a16b7ecbf5da0583b
Alejandra Izquierdo <allleizq@gmail.com>
2022-06-28 08:21:48 -0700

    questions array

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
+var startButton = document.getElementById("startbutton")
 var timerElement = document.querySelector(".timer");
+
+startButton.addEventListener("click", startsQuiz)
 
 var secondsLeft = 120;
 
@@ -14,3 +17,50 @@ function startTimer() {
 }
 
 startTimer();
+
+
+
+function startsQuiz() {
+    console.log("begin")
+    
+    goesToNextQ();
+    startTimer();
+}
+
+function goesToNextQ() {
+    
+}
+
+function selectsAnswer() {
+
+}
+
+var question1 = [
+  {
+    question: "Which of the following is not a popup box in Javascript?",
+    answers: [
+        { text: "prompt", incorrect: false },
+        { text: "array", incorrect: true },
+        { text: "alert", incorrect: false },
+        { text: "confirm", incorrect: false },
+    ]
+  },   
+  {
+    question: "Which of the following is a tag?",
+    answers: [
+        { text: "div", incorrect: true },
+        { text: "array", incorrect: false },
+        { text: "alert", incorrect: false },
+        { text: "scope", incorrect: false },
+    ]
+  }, 
+  {  
+  question: "Which of the following is used to style a webpage?",
+    answers: [
+        { text: "emojis", incorrect: true },
+        { text: "functions", incorrect: false },
+        { text: "methods", incorrect: false },
+        { text: "CSS", incorrect: false },
+    ]
+  }   
+]
