7e55e155dd1b4018f78bdbf48c2ba9c0963da7db
Alejandra Izquierdo <allleizq@gmail.com>
2022-06-28 23:51:58 -0700

    readMe material

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # timed-coding-quiz
 ## Description 
-
-##
-
+Hello, 
+Here you will find my first attempt to utilize JavaScript to make a quiz. I will be using:
+-HTML
+-CSS 
+-JAVASCRIPT 
+in order to develop a short coding quiz.
+Challenges: I am still trying to understand JavaScript and therefore struggled to create the best functional quiz. However, I do plan to continue to study in order to be able to fully understand how to utilize JavaScript. I plan to come back update my work!
 
 
 ## credits
-
 Used to learn how to sort through an array of questions randomly:
 https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+Used to nderstand Javascript:
+https://developer.mozilla.org/en-US/
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//created variables to reference html 
 var startButton = document.querySelector("#startbutton");
 var timerElement = document.querySelector("#timer");
 var highScores = document.querySelector("#highscores");
@@ -9,16 +10,16 @@ var startingBox = document.querySelector(".startingbox");
 var quizBox = document.querySelector("#quizbox");
 var answerButton = document.querySelector("#answer-button");
 var finishButton =document.querySelector("#finished")
-
+// created variables to use in order to shuffle questions, to move throughout page
 var shuffleQ 
 var currentQuestionIndex
-
+//added event listeners in order to call on specfic targets
 startButton.addEventListener("click", startsQuiz)
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++
     goesToNextQ()
 })
-
+//tells me what time my timer starts at and counts down from there
 var secondsLeft = 120;
 
 function startTimer() {
@@ -31,11 +32,10 @@ function startTimer() {
     }
  }, 1000);
 }
-
 startTimer();
 
 
-
+//used this function to begin quiz and to exit from starting page with rules of quiz
 function startsQuiz() {
     console.log("begin")
     startingBox.classList.add("deletes")
@@ -45,12 +45,13 @@ function startsQuiz() {
     goesToNextQ()
     startTimer()
 }
-
+//moves us to the next question in quiz
+//cleanSlate function removes items from previous question
 function goesToNextQ() {
     cleanSlate()
     displayQuestion(shuffleQ[currentQuestionIndex])   
 }
-
+//inserts question and answer arrary, in order to display on browser
 function displayQuestion(question) {
 questionsElement.innerText = question.question
 question.answers.forEach(answers => {
@@ -64,7 +65,8 @@ question.answers.forEach(answers => {
     answerButton.appendChild(button)
 })
 }
-
+//could not figure out how to successfully display when user selects correct answer 
+//or when they select incorrect answer
 function selectsAnswer() {
   
     if (shuffleQ.length > currentQuestionIndex + 1) {
@@ -72,7 +74,7 @@ function selectsAnswer() {
     }
        return finishButton;
     }
-
+//created a question array
 var questions = [{
     question: "Which of the following is not a popup box in Javascript?",
     answers: [
@@ -102,7 +104,7 @@ var questions = [{
   }   
 ]  
 
-
+//cleans out my previous question
 function cleanSlate() {
     nextButton.classList.add("deletes")
     while (answerButton.firstChild) {
